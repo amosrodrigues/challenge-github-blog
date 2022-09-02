03d97906a8c43aef5e3b4f87864d8892b665d8ce
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-09-01 22:15:00 -0300

    feat: implement header in component Post.

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,9 +1,73 @@
-import { PostContainer } from './styles'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
+import {
+  faArrowUpRightFromSquare,
+  faCalendarDay,
+  faChevronLeft,
+  faComment,
+} from '@fortawesome/free-solid-svg-icons'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Link } from 'react-router-dom'
+
+import {
+  BackLink,
+  DescriptionMarkdown,
+  GitHubLink,
+  PostContainer,
+  PostHeader,
+  PostInfo,
+  PostInfoItem,
+  PostNav,
+} from './styles'
 
 export function Post() {
   return (
     <PostContainer>
-      <h1>Post</h1>
+      <PostHeader>
+        <PostNav>
+          <BackLink as={Link} to="/" title="Voltar para o Blog">
+            <FontAwesomeIcon icon={faChevronLeft} fontSize={12} />
+            <span>VOLTAR</span>
+          </BackLink>
+
+          <GitHubLink
+            href="https://github.com/amosrodrigues"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span>VER NO GITHUB</span>
+            <FontAwesomeIcon icon={faArrowUpRightFromSquare} fontSize={12} />
+          </GitHubLink>
+        </PostNav>
+
+        <PostInfo>
+          <h2>JavaScript data types and data structures</h2>
+
+          <div>
+            <PostInfoItem>
+              <FontAwesomeIcon icon={faGithub} fontSize={18} />
+              <span>amosrodrigues</span>
+            </PostInfoItem>
+
+            <PostInfoItem>
+              <FontAwesomeIcon icon={faCalendarDay} fontSize={18} />
+              <span>Empresa</span>
+            </PostInfoItem>
+
+            <PostInfoItem>
+              <FontAwesomeIcon icon={faComment} fontSize={18} />
+              <span>31 segguidores</span>
+            </PostInfoItem>
+          </div>
+        </PostInfo>
+      </PostHeader>
+
+      <DescriptionMarkdown>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quisquam
+        iure magnam fugit consequuntur ab odio rerum, dolores, delectus quia
+        eveniet debitis quas quod corrupti enim amet, obcaecati eos expedita?
+      </DescriptionMarkdown>
     </PostContainer>
   )
 }
--- src/pages/Post/styles.ts
+++ src/pages/Post/styles.ts
@@ -1,3 +1,107 @@
 import styled from 'styled-components'
 
-export const PostContainer = styled.main``
+export const PostContainer = styled.div`
+  width: 100%;
+  max-width: 864px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`
+
+export const PostHeader = styled.header`
+  width: 100%;
+  height: 168px;
+
+  padding: 2rem;
+  margin-top: -5.5rem;
+
+  display: flex;
+  flex-direction: column;
+
+  gap: 1.25rem;
+
+  background: ${(props) => props.theme.profile};
+
+  box-shadow: 0px 2px 28px rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+`
+
+export const PostNav = styled.nav`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  position: relative;
+`
+
+export const BaseLink = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  color: ${(props) => props.theme.blue};
+  text-decoration: none;
+  border-bottom: 1px solid transparent;
+  position: absolute;
+
+  &:hover {
+    border-bottom: 1px solid ${(props) => props.theme.blue};
+  }
+
+  span {
+    font-weight: 700;
+    font-size: 0.75rem;
+    text-transform: uppercase;
+  }
+`
+
+export const BackLink = styled(BaseLink)`
+  left: 0;
+  top: 0;
+`
+
+export const GitHubLink = styled(BaseLink)`
+  right: 0;
+  top: 0;
+`
+
+export const PostInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  > h2 {
+    display: -webkit-box;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+
+    color: ${(props) => props.theme.title};
+    font-weight: 700;
+    font-size: 1.5rem;
+    line-height: 1.3;
+  }
+
+  > div {
+    display: flex;
+    gap: 1.5rem;
+  }
+`
+
+export const PostInfoItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  span {
+    color: ${(props) => props.theme.span};
+  }
+
+  svg {
+    color: ${(props) => props.theme.label};
+  }
+`
+
+export const DescriptionMarkdown = styled.section`
+  padding: 2.5rem 2rem;
+`
